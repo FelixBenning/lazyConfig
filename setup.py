7bbbf7ed4988180500346f70c3639932b43fc94d
Felix Benning <felix.benning@gmail.com>
2020-08-21 18:05:01 +0000

    successful upload to test.pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -11,11 +11,16 @@ setup(name='lazyConfig',
       author_email='felix.benning@gmail.com',
       description='lazily loading and overriding configuration for the lazy coder',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/FelixBenning/lazyConfig',
       packages=['lazyConfig' ],
       classifiers=[
-            "Programming Language :: Python :: 3"
-            "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)"
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
             "Development Status :: 4 - Beta"
+      ],
+      python_requires='>=3.8',
+      install_requires=[
+            'pyyaml>5<6'
       ]
      )
