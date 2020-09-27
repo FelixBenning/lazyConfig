54c4b941888c2700b4fb95ddea01a8a770861a0f
Felix Benning <felix.benning@gmail.com>
2020-09-27 14:35:56 +0000

    sphinx documentation

--- lazyConfig/factory.py
+++ lazyConfig/factory.py
@@ -1,6 +1,6 @@
 import os, yaml, json
 
-from typing import Dict, Callable, Union
+from typing import Dict, Callable, Union, List
 from _io import TextIOWrapper
 from collections.abc import Sequence, Mapping
 
@@ -8,19 +8,27 @@ from .config import Config, ConfigList
 from .lazyData import LazyList, LazyDict, LazyMode, DEFAULT_EXTENSION_MAP
 
 def from_env(
-    config: str = 'CONFIG', override: str = 'CONFIG_OVERRIDE',
+    config: str = 'CONFIG', 
+    override: str = 'CONFIG_OVERRIDE',
     laziness: LazyMode = LazyMode.CACHED,
     custom_extension_loader: Dict[str, Callable[[TextIOWrapper], Union[dict, list]]] = {}
 ) -> Config:
     """ build Config from environment variables
 
-    config: (required) environment variable with path to (default) configuration
-                directory as its contents
-    override: env var pointing to os.pathsep (':' linux, ';' win) separated paths
-                configuration directories overriding the `config` directory
-    laziness: a mode from the enum LazyMode
-    custom_extension_loader: a dictionary of file extensions and loader functions
-                overriding the default loaders. E.g. {'yml': yaml.safe_load}
+    Args:
+        config (str, optional): environment variable with path to (default) configuration
+                directory as its contents. Defaults to 'CONFIG'.
+        override (str, optional): env var pointing to os.pathsep (':' linux, ';' win) separated paths
+                configuration directories overriding the `config` directory. 
+                Defaults to 'CONFIG_OVERRIDE'.
+        laziness (LazyMode, optional): a mode from the enum LazyMode. Defaults to LazyMode.CACHED.
+        custom_extension_loader (Dict[str, Callable[[TextIOWrapper], Union[dict, list]]], optional): 
+                a dictionary of file extensions and loader functions
+                overriding the default loaders. E.g. {'yml': yaml.safe_load}. Defaults to {}.
+
+    Returns:
+        lazyConfig.Config 
+
     """
     override_list = []
 
@@ -35,19 +43,23 @@ def from_env(
     )
 
 def from_path(
-    config: str, override: list = [],
+    config: str, override: List[str] = [],
     laziness: LazyMode = LazyMode.CACHED,
     custom_extension_loader: Dict[str, Callable[[TextIOWrapper], Union[dict, list]]] = {}
 ) -> Config:
-    """ build Config from path to configuration directories
+    """build Config from path to configuration directories
 
-    config: (required) path to (default) configuration directory as its
-                contents
-    override: list of paths to configuration directories overriding the
-                `config` directory
-    laziness: a mode from the enum LazyMode
-    custom_extension_loader: a dictionary of file extensions and loader functions
-                overriding the default loaders. E.g. {'yml': yaml.safe_load}   
+    Args:
+        config (str): path to (default) configuration directory as its contents
+        override (List[str], optional): list of paths to configuration directories overriding the
+                `config` directory. Defaults to [].
+        laziness (LazyMode, optional): a mode from the enum LazyMode. Defaults to LazyMode.CACHED.
+        custom_extension_loader (Dict[str, Callable[[TextIOWrapper], Union[dict, list]]], optional): 
+                a dictionary of file extensions and loader functions
+                overriding the default loaders. E.g. {'yml': yaml.safe_load}. Defaults to {}.
+
+    Returns:
+        lazyConfig.Config
     """
     ext_map = DEFAULT_EXTENSION_MAP.copy()
     ext_map.update(custom_extension_loader)
@@ -57,11 +69,25 @@ def from_path(
         override = [LazyDict(x, laziness, extension_loader) for x in override]
     ))
 
-def from_primitive(config, override: list = []) -> Config:
-    """ build Config from primitive datatypes (dict/list)
+def from_primitive(
+    config: Union[Mapping, Sequence], 
+    override: List[Union[Mapping, Sequence]] = []
+) -> Config:
+    """build Config from primitive datatypes (dict/list)
 
-    note: if config is a list, the last non-empty list in override is used an
-    all other are ignored
+    Args:
+        config (Union[Mapping, Sequence]): default configuration
+        override (List[Union[Mapping, Sequence]], optional): list of overriding
+            configurations. Defaults to [].
+
+    Raises:
+        ValueError: if config is neither Mapping nor Sequence
+
+    Returns:
+        lazyConfig.Config
+
+    note: if config is a list, the last non-empty list in override is used and
+    all others are ignored
     """
     if isinstance(config, Mapping):
         return Config(
