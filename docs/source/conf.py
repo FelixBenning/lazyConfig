bc92f766bc7313415ea74a5edc811e28b33062b7
Felix Benning <felix.benning@d-fine.de>
2020-09-28 21:29:25 +0000

    hotfix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('__file__/../../lazyConfig'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
